7e1f3a07929e46dd8486f4ac6594e1d50520b5c9
Denis <ezhdenis@yandex.ru>
2020-03-12 16:32:15 +0300

    Update Application.java

--- src/main/java/ru/ezhov/dbcommandexecutor/Application.java
+++ src/main/java/ru/ezhov/dbcommandexecutor/Application.java
@@ -7,46 +7,48 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Application {
-    private static final Logger LOG = Logger.getLogger(Application.class.getName());
+	private static final Logger LOG = Logger.getLogger(Application.class.getName());
 
-    public static void main(String[] args) {
-        String driverClass = System.getProperty("db.command.executor.driverClass");
-        String url = System.getProperty("db.command.executor.url");
-        Properties properties = properties("db.command.executor.prop");
-        String delimiter = System.getProperty("db.command.executor.queries.delimiter", "___");
-        String[] queries = queries("db.command.executor.queries", delimiter);
+	public static void main(String[] args) {
+		String driverClass = System.getProperty("db.command.executor.driverClass");
+		String url = System.getProperty("db.command.executor.url");
+		Properties properties = properties("db.command.executor.prop");
+		String delimiter = System.getProperty("db.command.executor.queries.delimiter", "___");
+		boolean isIterate = Boolean.parseBoolean(System.getProperty("db.command.executor.isIterate", "false"));
+		String[] queries = queries("db.command.executor.queries", delimiter);
 
-        DbCommandExecutor dbCommandExecutor = new DbCommandExecutor(driverClass, url, properties);
-        try {
-            dbCommandExecutor.execute(queries);
-        } catch (DbCommandExecutorException e) {
-            LOG.log(
-                    Level.SEVERE,
-                    "Ошибка выполнения запроса(ов)." +
-                            " driverClass: " + driverClass +
-                            " url: " + url +
-                            " properties: " + properties.stringPropertyNames() +
-                            " queries: " + Arrays.asList(queries),
-                    e
-            );
-        }
-    }
+		DbCommandExecutor dbCommandExecutor = new DbCommandExecutor(driverClass, url, properties);
+		try {
+			dbCommandExecutor.execute(isIterate, queries);
+		} catch (DbCommandExecutorException e) {
+			LOG.log(
+				Level.SEVERE,
+				"Ошибка выполнения запроса(ов)." +
+					" driverClass: " + driverClass +
+					" url: " + url +
+					" isIterate: " + isIterate +
+					" properties: " + properties.stringPropertyNames() +
+					" queries: " + Arrays.asList(queries),
+				e
+			);
+		}
+	}
 
-    private static String[] queries(String property, String delimiter) {
-        String queriesProperty = System.getProperty(property);
-        return queriesProperty.split(delimiter);
-    }
+	private static String[] queries(String property, String delimiter) {
+		String queriesProperty = System.getProperty(property);
+		return queriesProperty.split(delimiter);
+	}
 
-    private static Properties properties(String property) {
-        Properties properties = System.getProperties();
-        Set<String> propertyNames = properties.stringPropertyNames();
-        Properties prop = new Properties();
-        for (String name : propertyNames) {
-            if (name.startsWith(property)) {
-                String s = name.substring(property.length() + 1 /*точка*/);
-                prop.setProperty(s, properties.getProperty(name));
-            }
-        }
-        return prop;
-    }
+	private static Properties properties(String property) {
+		Properties properties = System.getProperties();
+		Set<String> propertyNames = properties.stringPropertyNames();
+		Properties prop = new Properties();
+		for (String name : propertyNames) {
+			if (name.startsWith(property)) {
+				String s = name.substring(property.length() + 1 /*точка*/);
+				prop.setProperty(s, properties.getProperty(name));
+			}
+		}
+		return prop;
+	}
 }
