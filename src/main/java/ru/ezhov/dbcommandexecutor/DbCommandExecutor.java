d5b7a29dd559ba00d522083a057bb94644966b89
ezhov_da <ezhdenis@yandex.ru>
2020-03-12 21:49:25 +0300

    chore: откорректированы отступы

--- src/main/java/ru/ezhov/dbcommandexecutor/DbCommandExecutor.java
+++ src/main/java/ru/ezhov/dbcommandexecutor/DbCommandExecutor.java
@@ -7,40 +7,40 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DbCommandExecutor {
-	private String driverClass;
-	private String url;
-	private Properties properties;
+    private String driverClass;
+    private String url;
+    private Properties properties;
 
-	public DbCommandExecutor(String driverClass, String url, Properties properties) {
-		this.driverClass = driverClass;
-		this.url = url;
-		this.properties = properties;
-	}
+    public DbCommandExecutor(String driverClass, String url, Properties properties) {
+        this.driverClass = driverClass;
+        this.url = url;
+        this.properties = properties;
+    }
 
-	public void execute(boolean isIterate, String[] queries) throws DbCommandExecutorException {
-		String query = null;
-		try {
-			Class.forName(driverClass);
-			try (Connection connection = DriverManager.getConnection(url, properties)) {
-				for (String q : queries) {
-					try (Statement statement = connection.createStatement();) {
-						query = q;
-						long begin = System.currentTimeMillis();
-						final boolean execute = statement.execute(query);
-						if (isIterate && execute) {
-							try (final ResultSet resultSet = statement.getResultSet();) {
-								while (resultSet.next()) {
-									resultSet.getObject(1);
-								}
-							}
-						}
-						long end = System.currentTimeMillis();
-						System.out.printf("time\t%s ms\tquery\t%s\n", (end - begin), query);
-					}
-				}
-			}
-		} catch (Exception e) {
-			throw new DbCommandExecutorException("Ошибка выполнения запроса: " + query, e);
-		}
-	}
+    public void execute(boolean isIterate, String[] queries) throws DbCommandExecutorException {
+        String query = null;
+        try {
+            Class.forName(driverClass);
+            try (Connection connection = DriverManager.getConnection(url, properties)) {
+                for (String q : queries) {
+                    try (Statement statement = connection.createStatement()) {
+                        query = q;
+                        long begin = System.currentTimeMillis();
+                        final boolean execute = statement.execute(query);
+                        if (isIterate && execute) {
+                            try (final ResultSet resultSet = statement.getResultSet()) {
+                                while (resultSet.next()) {
+                                    resultSet.getObject(1);
+                                }
+                            }
+                        }
+                        long end = System.currentTimeMillis();
+                        System.out.printf("time\t%s ms\tquery\t%s\n", (end - begin), query);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DbCommandExecutorException("Ошибка выполнения запроса: " + query, e);
+        }
+    }
 }
